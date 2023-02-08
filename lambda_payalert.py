64ba11a99ed16f63249261dd8c15a32a08b4ad30
Connor T Werdel <75349974+c-werdel@users.noreply.github.com>
2023-02-08 16:45:00 -0700

    Update lambda_payalert.py

--- lambda_payalert.py
+++ lambda_payalert.py
@@ -40,15 +40,19 @@ def lambda_handler(event, context):
 		pass
 
 	if worked:
-		#Do only the schedule call to DISABLED nothing more!
+		client = boto3.client('scheduler',  region_name='us-east-1')
 		response = client.update_schedule(
-			FlexibleTimeWindow={
-				'Mode': 'OFF'
-			},
-		Name='payalert_reminder', 
-		ScheduleExpression='cron(30 * * * ? *)',
-		State='DISABLED',
-		)
+	    FlexibleTimeWindow={
+	        'Mode': 'OFF'
+	    },
+	    Name='payalert_reminder', 
+	    ScheduleExpression='cron(30 * * * ? *)',
+	    State='DISABLED',
+	    Target= {
+	        'Arn': 'arn:aws:lambda:us-east-1:297098627551:function:Slack_payroll_notification',
+	        'RoleArn': 'arn:aws:iam::297098627551:role/service-role/Amazon_EventBridge_Scheduler_LAMBDA_payalert_reminder_a330188cab'
+	    }
+	)
 	else: 
 		current_week = mydoc()
 		work_week = current_week['Work Week']
@@ -141,7 +145,7 @@ def lambda_handler(event, context):
 	    },
 	    Name='payalert_reminder', 
 	    ScheduleExpression='cron(30 * * * ? *)',
-	    State='DISABLED',
+	    State='ENABLED',
 	    Target= {
 	        'Arn': 'arn:aws:lambda:us-east-1:297098627551:function:Slack_payroll_notification',
 	        'RoleArn': 'arn:aws:iam::297098627551:role/service-role/Amazon_EventBridge_Scheduler_LAMBDA_payalert_reminder_a330188cab'
