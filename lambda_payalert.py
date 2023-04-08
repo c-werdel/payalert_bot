3f7d3dd9d2e9dc062252037691b485705430cc92
Connor T Werdel <75349974+c-werdel@users.noreply.github.com>
2023-04-08 12:21:38 -0400

    Update lambda_payalert.py
    syntax change

--- lambda_payalert.py
+++ lambda_payalert.py
@@ -38,103 +38,103 @@ def lambda_handler(event, context):
 				worked = True
 	except:
 		pass
+	
+	
+	if worked:
+		# Do only the schedule call to DISABLED nothing more!
+		client = boto3.client('scheduler', region_name='us-east-1')
+		response = client.update_schedule(
+		    FlexibleTimeWindow={
+			'Mode': 'OFF'
+		    },
+		    Name='payalert_reminder', 
+		    ScheduleExpression='cron(30 * * * ? *)',
+		    State='DISABLED',
+		    Target= {
+			'Arn': 'arn:aws:lambda:us-east-1:297098627551:function:Slack_payroll_notification',
+			'RoleArn': 'arn:aws:iam::297098627551:role/service-role/Amazon_EventBridge_Scheduler_LAMBDA_payalert_reminder_a330188cab'
+		    }
+		)
+		print("Thank you, 15 min Alarm deactivated.")
+		
+	else:
+		current_week = mydoc()
+		work_week = current_week['Work Week']
+		Dates = current_week['Dates']
+		Total_Hours = current_week['Total Hours']
+		Total_Pay = current_week['Total Pay']
 
-   if worked:
-        # Do only the schedule call to DISABLED nothing more!
-        client = boto3.client('scheduler',  region_name='us-east-1')
-        response = client.update_schedule(
-            FlexibleTimeWindow={
-                'Mode': 'OFF'
-            },
-            Name='payalert_reminder', 
-            ScheduleExpression='cron(30 * * * ? *)',
-            State='DISABLED',
-            Target= {
-                'Arn': 'arn:aws:lambda:us-east-1:297098627551:function:Slack_payroll_notification',
-                'RoleArn': 'arn:aws:iam::297098627551:role/service-role/Amazon_EventBridge_Scheduler_LAMBDA_payalert_reminder_a330188cab'
-            }
-        )
-        print("Thank you, 15 min Alarm deactivated.")    
-	
-   else:
-	current_week = mydoc()
-        work_week = current_week['Work Week']
-        Dates = current_week['Dates']
-        Total_Hours = current_week['Total Hours']
-        Total_Pay = current_week['Total Pay']
+		hook_url = os.environ["webhook"]                   
+		payload = {    
+		    "blocks": [
+			{
+			    "type": "header",
+			    "text": {
+				"type": "plain_text",
+				"text": "Pay Roll Alert\n"
+			    }
+			},
+			{
+			    "type": "section",
+			    "fields": [
+				{
+				    "type": "mrkdwn",
+				    "text": "For Connor Werdel"
+				}
+			    ]
+			},
+			{
+			    "type": "section",
+			    "fields": [
+				{
+				    "type": "mrkdwn",
+				    "text": f"*Work Week:*\n {work_week} {Dates}"
+				}
+			    ]
+			},
+			{
+			    "type": "section",
+			    "fields": [
+				{
+				    "type": "mrkdwn",
+				    "text": f"*Total Hours: *\n {Total_Hours}"
+				}
+			    ]
+			},
+			{
+			    "type": "section",
+			    "fields": [
+				{
+				    "type": "mrkdwn",
+				    "text": f"*Paid Amount:*\n{Total_Pay}"
+				}
+			    ]
+			},
+			{
+			    "type": "section",
+			    "text": {
+				"type": "mrkdwn",
+				"text": f"*Pay Roll Sheet:*\n<https://docs.google.com/spreadsheets/d/{gsheetid}/edit#gid=1445946700|Link>"
+			    }
+			},
+			{
+			    "type": "actions",
+			    "block_id": "actionblock789",
+			    "elements": [
+				{
+				    "type": "button",
+				    "text": {
+					"type": "plain_text",
+					"text": "Payment complete"
+				    },
+				    "style": "primary",
+				    "value": "click_me_123"
+				}             
+			    ]            
+			}
+		    ]
+		}
 
-        hook_url = os.environ["webhook"]                   
-        payload = {    
-            "blocks": [
-                {
-                    "type": "header",
-                    "text": {
-                        "type": "plain_text",
-                        "text": "Pay Roll Alert\n"
-                    }
-                },
-                {
-                    "type": "section",
-                    "fields": [
-                        {
-                        "type": "mrkdwn",
-                        "text": "For Connor Werdel"
-                    }
-                    ]
-                },
-                {
-                    "type": "section",
-                    "fields": [
-                        {
-                            "type": "mrkdwn",
-                            "text": f"*Work Week:*\n {work_week} {Dates}"
-                        }
-                    ]
-                },
-                {
-                    "type": "section",
-                    "fields": [
-                        {
-                            "type": "mrkdwn",
-                            "text": f"*Total Hours: *\n {Total_Hours}"
-                        }
-                    ]
-                },
-                {
-                    "type": "section",
-                    "fields": [
-                        {
-                            "type": "mrkdwn",
-                            "text": f"*Paid Amount:*\n{Total_Pay}"
-                        }
-                    ]
-                },
-                {
-                    "type": "section",
-                    "text": {
-                        "type": "mrkdwn",
-                        "text": f"*Pay Roll Sheet:*\n<https://docs.google.com/spreadsheets/d/{gsheetid}/edit#gid=1445946700|Link>"
-                    }
-                },
-                {
-                "type": "actions",
-                "block_id": "actionblock789",
-                "elements": [
-                    {
-                        "type": "button",
-                        "text": {
-                            "type": "plain_text",
-                            "text": "Payment complete"
-                        },
-                        "style": "primary",
-                        "value": "click_me_123"
-                    }             
-                ]            
-                }
-            ]
-        }
-	
-	
 	encoded_data = json.dumps(payload).encode('utf-8')
         r = http.request( 
             'POST',
